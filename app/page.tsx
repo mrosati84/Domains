be6581c55a0e882610cd0f8ae0c70b76a1c41d0a
Matteo Rosati <rosati.matteo@gmail.com>
2025-09-18 17:42:03 +0200

    add middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import { headers } from "next/headers";
+// import { headers } from "next/headers";
 
 export default async function Home() {
-  const h = await headers();
-  const host = h.get("host");
-  const protocol = h.get("x-forwarded-proto") || "http";
-  const fullUrl = `${protocol}://${host}${h.get("x-invoke-path") || ""}`;
+  // const h = await headers();
+  // const host = h.get("host");
+  // const protocol = h.get("x-forwarded-proto") || "http";
+  // const fullUrl = `${protocol}://${host}${h.get("x-invoke-path") || ""}`;
 
-  return <div>{fullUrl}</div>;
+  console.log("CONTROLLER");
+
+  return <h1>Hello</h1>
 }
---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+    // esempio: log della URL richiesta
+    console.log("Requested URL:", request.nextUrl.href)
+
+    // puoi modificare la response, fare rewrite, redirect, ecc.
+    return NextResponse.next()
+}
+
+// opzionale: limitare le route dove applicarlo
+export const config = {
+    matcher: ['/'],
+}
