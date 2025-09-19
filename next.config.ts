00797f977cde07757eaa053acfb5e62156806213
Matteo Rosati <matteo.rosati@akqa.com>
2025-09-19 11:59:06 +0200

    add fr redirect

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "fr.mrosati.it",
+          },
+        ],
+        destination: "/fr",
+        permanent: true,
+      },
+    ];
+  },
   /* config options here */
   async rewrites() {
     return [
@@ -9,23 +24,23 @@ const nextConfig: NextConfig = {
         has: [
           {
             type: "host",
-            value: "de.mrosati.it"
-          }
+            value: "de.mrosati.it",
+          },
         ],
-        destination: "/:lang/de/:path*"
+        destination: "/:lang/de/:path*",
       },
       {
         source: "/:lang/:path*",
         has: [
           {
             type: "host",
-            value: "fr.mrosati.it"
-          }
+            value: "fr.mrosati.it",
+          },
         ],
-        destination: "/:lang/fr/:path*"
-      }
+        destination: "/:lang/fr/:path*",
+      },
     ];
-  }
+  },
 };
 
 export default nextConfig;
